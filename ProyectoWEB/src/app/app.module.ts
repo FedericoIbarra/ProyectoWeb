03805a6cef52ec510fec1269315696a8cd9d8b0c
BernardoElizondo <is708387@iteso.mx>
2019-04-24 17:39:40 -0500

    Se creó el componente de información nutrimental. Falta hacer la tabla.

--- ProyectoWEB/src/app/app.module.ts
+++ ProyectoWEB/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoNutrimentalComponent } from './info-nutrimental/info-nutrimental.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IngredientesComponent
+    InfoNutrimentalComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoWEB/src/app/info-nutrimental/info-nutrimental.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-nutrimental',
+  templateUrl: './info-nutrimental.component.html',
+  styleUrls: ['./info-nutrimental.component.css']
+})
+export class InfoNutrimentalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
