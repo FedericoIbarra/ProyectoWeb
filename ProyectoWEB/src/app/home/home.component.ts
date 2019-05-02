161a32fce293105a686dd038bece0f0e9aaa8092
FedericoIbarra <is702981@iteso.mx>
2019-05-01 21:22:27 -0500

    Fix

--- ProyectoWEB/src/app/home/home.component.ts
+++ ProyectoWEB/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PorcionesService } from './porciones.service';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _porcionesService: PorcionesService, private _login: LoginService) { }
 
   ngOnInit() {
+    let user = this._login.getUser();
+    this._porcionesService.setPorciones(user.planElegido.porciones);
   }
 
 }
