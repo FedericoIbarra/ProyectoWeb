70dd78bed738f290d5d92bf26ac4339b4022fff4
BernardoElizondo <is708387@iteso.mx>
2019-05-01 20:18:35 -0500

    Información Nutrimental completa

--- ProyectoWEB/src/app/info-nutrimental/info-nutrimental.component.ts
+++ ProyectoWEB/src/app/info-nutrimental/info-nutrimental.component.ts
@@ -9,13 +9,39 @@ import { IngredientesService} from '../info-nutrimental/ingredientes.service'
 })
 export class InfoNutrimentalComponent implements OnInit {
 
+  public carneBajoContenidoGrasas: Ingredientes[] = [];
+  public carneMedioContenidoGrasas: Ingredientes[] = [];
+  public carneAltoContenidoGrasas: Ingredientes[] = [];
+  public cereales: Ingredientes[] = [];
+  public jugos: Ingredientes[] = [];
+  public frutasBajoContenidoFibra: Ingredientes[] = [];
+  public frutasMedioContenidoFibra: Ingredientes[] = [];
+  public frutasAltoContenidoFibra: Ingredientes[] = [];
+  public verdurasGrupoA: Ingredientes[] = [];
+  public verdurasGrupoB: Ingredientes[] = [];
+  public lacteos: Ingredientes[] = [];
+  public leguminosas: Ingredientes[] = [];
+  public grasas: Ingredientes[] = [];
   public azucares: Ingredientes[] = [];
-  public libres: Ingredientes[] = [];
+  public libres: Ingredientes[] = [];  
   
 
   constructor(private ingredientesService: IngredientesService) { }
 
   ngOnInit() {
+    this.carneBajoContenidoGrasas = this.ingredientesService.getCarnesBajoContenidoGrasas();
+    this.carneMedioContenidoGrasas = this.ingredientesService.getCarnesMedioContenidoGrasas();
+    this.carneAltoContenidoGrasas = this.ingredientesService.getCarnesAltoContenidoGrasas();
+    this.cereales = this.ingredientesService.getCereales();
+    this.jugos = this.ingredientesService.getJugos();
+    this.frutasBajoContenidoFibra = this.ingredientesService.getFrutasBajoContenidoFibra();
+    this.frutasMedioContenidoFibra = this.ingredientesService.getFrutasMedioContenidoFibra();
+    this.frutasAltoContenidoFibra = this.ingredientesService.getFrutasAltoContenidoFibra();
+    this.verdurasGrupoA = this.ingredientesService.getVerdurasGrupoA();
+    this.verdurasGrupoB = this.ingredientesService.getVerdurasGrupoB();
+    this.lacteos = this.ingredientesService.getLacteos();
+    this.leguminosas = this.ingredientesService.getLeguminosas();
+    this.grasas = this.ingredientesService.getGrasas();
     this.azucares = this.ingredientesService.getAzucares();
     this.libres = this.ingredientesService.getLibres();
   }
